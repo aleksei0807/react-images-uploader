1ed237fce3c1a0d8d642156a0694ab562d4d9a57
Алексей <a@guava.by>
2016-05-19 23:06:00 +0300

    create CustomComponent (react components example)

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class CustomComponent extends Component {
+    render() {
+        return (
+            <div className="custom-component">
+				Custom Component
+			</div>
+        );
+    }
+}
