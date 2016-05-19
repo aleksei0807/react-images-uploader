f1e2dd38d2689c41e43eb71dab475034f0088f66
Алексей <a@guava.by>
2016-05-19 23:14:18 +0300

    create default example

---
+++ examples/src/default.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CustomComponent from '../../lib/index';
+
+ReactDOM.render(
+	<CustomComponent />,
+	document.getElementById('root')
+);
