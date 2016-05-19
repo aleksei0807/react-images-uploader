eb529e8a3649f100afb511c19d8528cd046fb650
Алексей <a@guava.by>
2016-05-19 23:13:31 +0300

    Create examples list

---
+++ examples/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+	<ul>
+		<li><a href="./pages/default.html">Default theme</a></li>
+		<li><a href="./pages/custom.html">Custom theme</a></li>
+	</ul>,
+	document.getElementById('root')
+);
