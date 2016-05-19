8ae9cd13ea119d7f3d729d7e746b2147368b504a
Алексей <a@guava.by>
2016-05-19 23:15:51 +0300

    add gulp configuration

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var stylus = require('gulp-stylus');
+
+gulp.task('styles', function () {
+  return gulp.src('./styles/*.styl')
+    .pipe(stylus({
+      compress: true
+    }))
+    .pipe(gulp.dest('./styles'))
+    .pipe(gulp.dest('./examples/build'));
+});
+
+gulp.task('themes', function () {
+  return gulp.src('./styles/themes/**/*.styl')
+    .pipe(stylus({
+      compress: true
+    }))
+    .pipe(gulp.dest('./styles/themes'))
+    .pipe(gulp.dest('./examples/build/themes'));
+});
+
+gulp.task('default', ['styles', 'themes']);
