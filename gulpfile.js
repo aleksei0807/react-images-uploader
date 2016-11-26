3547276d226c3d26b00849a6468fc4a0500ca2de
Алексей <a@guava.by>
2016-11-26 14:22:23 +0300

    stylus to postcss

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,56 @@
+/* eslint-disable */
 var gulp = require('gulp');
-var stylus = require('gulp-stylus');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var functions = require('postcss-functions');
+var precss = require('precss');
+var atImport = require("postcss-import");
+var easyImport = require('postcss-easy-import');
+var postCssModules = require('postcss-modules');
+var cssnano = require('cssnano');
+
+var processors = [
+	atImport({
+		plugins: [easyImport],
+	}),
+	postCssModules({
+		scopeBehaviour: 'global',
+		generateScopedName: '[name]__[local]___[hash:base64:5]',
+	}),
+	autoprefixer,
+	precss({
+		variables: {
+			variables: require('./src/styles/vars.css')
+		}
+	}),
+	functions({
+		functions: require('./src/styles/funcs.css')
+	}),
+	cssnano()
+];
 
 gulp.task('styles', function () {
-  return gulp.src('./styles/*.styl')
-    .pipe(stylus({
-      compress: true
-    }))
+  return gulp.src('./src/styles/*.css')
+    .pipe(postcss(processors))
     .pipe(gulp.dest('./styles'))
     .pipe(gulp.dest('./examples/build'));
 });
 
 gulp.task('themes', function () {
-  return gulp.src('./styles/themes/**/*.styl')
-    .pipe(stylus({
-      compress: true
-    }))
+  return gulp.src('./src/styles/themes/**/*.css')
+    .pipe(postcss(processors))
     .pipe(gulp.dest('./styles/themes'))
     .pipe(gulp.dest('./examples/build/themes'));
 });
 
 gulp.task('examples-page-styles', function () {
-  return gulp.src('./examples/style.styl')
-    .pipe(stylus({
-      compress: true
-    }))
+  return gulp.src('./examples/src/style.css')
+    .pipe(postcss(processors))
     .pipe(gulp.dest('./examples'));
 });
 
-gulp.task('default', ['styles', 'themes']);
+gulp.task('default', ['styles', 'themes', 'examples-page-styles']);
 
 gulp.task('watch', function () {
-	gulp.watch(['./styles/*.styl', './styles/themes/**/*.styl'], ['default']);
+	gulp.watch(['./styles/*.css', './styles/themes/**/*.css', './examples/src/style.css'], ['default']);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,20 @@
+/* eslint-disable */
 const webpack = require('webpack');
 const RemoveWebpackPlugin = require('remove-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const functions = require('postcss-functions');
+const precss = require('precss');
+const atImport = require("postcss-import");
+const easyImport = require('postcss-easy-import');
+const postCssModules = require('postcss-modules');
+
+const postCssLoader = [
+	'css-loader?modules',
+	'&importLoaders=1',
+	'&localIdentName=[name]__[local]___[hash:base64:5]',
+	'&disableStructuralMinification',
+	'!postcss-loader'
+];
 
 module.exports = {
     entry: {
@@ -14,6 +29,7 @@ module.exports = {
 	plugins: [
 		new RemoveWebpackPlugin('./build'),
 		new webpack.NoErrorsPlugin(),
+		new webpack.optimize.DedupePlugin(),
 		new webpack.optimize.UglifyJsPlugin({
 	      compress: {
 	        warnings: false,
@@ -41,7 +57,31 @@ module.exports = {
 		  },
 		  {
 			  test: /\.js$/, loader: "react-hot-loader!babel-loader", exclude: [/node_modules/, /lib/]
+		  },
+		  {
+			  test: /\.css$/,
+			  loader: postCssLoader.join('')
 		  }
 	  ]
+	},
+	postcss: function() {
+		return [
+			atImport({
+				plugins: [easyImport],
+			}),
+			postCssModules({
+				scopeBehaviour: 'global',
+				generateScopedName: '[name]__[local]___[hash:base64:5]',
+			}),
+			autoprefixer,
+			precss({
+				variables: {
+					variables: require('./src/styles/vars.css')
+				}
+			}),
+			functions({
+				functions: require('./src/styles/funcs.css')
+			})
+		];
 	}
 };
