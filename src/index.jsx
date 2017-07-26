5361036bb6a19ab2cb246a274458279a97457748
Patrick Johnson <patrick@jedioftheco.de>
2017-07-26 17:05:37 +0200

    PropTypes to import from prop-types package to remove deprecation warnings

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 /* @flow */
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import fetch from 'isomorphic-fetch';
 import autobind from 'autobind-decorator';
 import classnames from 'classnames';
